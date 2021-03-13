a2851765cc7f4826f83cbcc762083b6371792c73
-人生巅峰财务自由 <469509908@qq.com>
2021-03-13 13:54:30 +0800

    init

---
+++ Aggregator/MyAggregator.cs
@@ -0,0 +1,35 @@
+﻿using Ocelot.Middleware;
+using Ocelot.Multiplexer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Net.Http;
+
+namespace ApiGateWay.Aggregator
+{
+    public class MyAggregator:IDefinedAggregator
+    {
+        public MyAggregator()
+        { 
+        
+        }
+        public async Task<DownstreamResponse> Aggregate(List<HttpContext> responses)
+        {
+            List<string> results = new List<string>();
+            foreach (var context in responses)
+            {
+                var result =await context.Items.DownstreamResponse().Content.ReadAsStringAsync();
+                results.Add(result);
+            }
+            var merge=string.Join(";",results.ToArray());
+            var stringContent = new StringContent(merge);
+            var headers = new List<Header>();
+            return new DownstreamResponse(stringContent, HttpStatusCode.OK, headers, "some reason");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ApiGateWay
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                        .AddJsonFile("ocelot.json",true,true)
+                        .AddEnvironmentVariables();
+                })
+               .ConfigureLogging((hostingContext, logging) =>
+               {
+                   //TODO - Add logging
+                   logging.AddConsole();
+               })
+               .UseStartup<Startup>();
+    }
+}
