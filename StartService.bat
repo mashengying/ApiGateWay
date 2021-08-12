afd0ceef4f711a51d7dde716d4d1cd7328ab2789
mashengying <469509908@qq.com>
2021-08-12 15:54:37 +0800

    test service batch

--- StartService.bat
+++ StartService.bat
@@ -1,16 +1,16 @@
-set appName="ApiGateWay"
-set binPath="D:\AutomationTools\Jenkins\.jenkins\workspace\"%appName%
+set appName=ApiGateWay
+set binPath="D:\AutomationTools\Jenkins\.jenkins\workspace\+%appName%"
 sc query %appName%
 if %errorlevel%==0 (
-    echo "found the service" %appName%
+    echo found the service %appName%
 	sc stop %appName%
-	echo "stop the service" %appName%
+	echo stop the service %appName%
 	sc start %appName%
-	echo "start the service" %appName%
+	echo start the service %appName%
 ) else (
-	echo "Not Found The Service" %appName%
+	echo Not Found The Service %appName%
 	sc create %appName% binPath= %binPath%
-	echo "create service" %appName%
+	echo create service %appName%
 	sc start %appName%
-	echo "start the service" %appName%
+	echo start the service %appName%
 )
