ba7e04bf16481f4aded1a861080cee07aac4d33a
mashengying <469509908@qq.com>
2021-08-12 15:39:49 +0800

    test service batch

--- StartService.bat
+++ StartService.bat
@@ -1,15 +1,15 @@
 set appName="ApiGateWay"
 sc query %appName%
 if %errorlevel%==0 (
-    echo "found the service %appName%"
+    echo "found the service" %appName%
 	sc stop %appName%
-	echo "stop the service %appName%"
+	echo "stop the service" %appName%
 	sc start %appName%
-	echo "start the service %appName%"
+	echo "start the service" %appName%
 ) else (
-	echo "Not Found The Service"
-	sc create %appName% binPath= "..publish\%appName%.exe"
-	echo "create service %appName%"
+	echo "Not Found The Service" %appName%
+	sc create %appName% binPath= "..\publish\%appName%.exe"
+	echo "create service" %appName%
 	sc start %appName%
-	echo "start the service %appName%"
+	echo "start the service" %appName%
 )
