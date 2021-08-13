60fb6400dbcd93b8d3eee463024540afd1fb6abd
mashengying <469509908@qq.com>
2021-08-13 10:30:21 +0800

    test jenkin service batch

--- StartService.bat
+++ StartService.bat
@@ -1,12 +1,11 @@
-set appName=ApiGateWay
+::find filename with .csproj or .sln extension
+for /f "delims=" %%a in ('dir /b "*.csproj";"*.sln"') do set appName= %%~na
+echo %appName%
+::set service binPath
 set binPath="D:\AutomationTools\Jenkins\.jenkins\workspace\publish\%appName%\%appName%.exe"
 sc query %appName%
 if %errorlevel%==0 (
     echo [app]Found The Service %appName%
-	:sc stop %appName%
-	:echo [app]Stop The Service %appName%
-	:sc start %appName%
-	:echo [app]Start The Service %appName%
 	powershell -Command "& {Restart-Service %appName%}"
 	exit 0
 ) else (
